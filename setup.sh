27cb589b87c07ddbf00905b80c98b2f7c481d337
bit-turtle <109782318+bit-turtle@users.noreply.github.com>
2023-10-20 19:13:25 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+echo "HZ7 RPi Setup"
+
+confirm() {
+	input=""
+	read -p "Install $1? [Y/N]: " input
+	if [ $input == "Y" ]
+	then
+		return 0 #Yes
+	elif [ $input == "y" ]
+		return 0 #Yes
+	elif [ $input == "Yes" ]
+		return 0 #Yes
+	elif [ $input == "yes" ]
+		return 0 #Yes
+	elif [ $input == "YES" ]
+		return 0 #Yes
+	else
+		return 1 #No
+	fi
+}
+
+confirm "OpenGL [GLFWlib & GLEWlib]"
+if [ $? -eq 0 ]
+then
+#Setup OpenGL
+echo "Installng OpenGL..."
+#GLFW
+echo "Installing GLFW..."
+cd ~ #Main Folder
+git clone https://github.com/glfw/glfw.git
+cd ./glfw
+echo "Setting Up Dependencies"
+sudo apt install xorg-dev
+sudo apt-get -y install cmake #Get CMake
+echo "Building..."
+cmake -G "Unix Makefiles"
+echo "Done!"
+#GLEW
+echo "Installing GLEW..."
+sudo apt-get install -y libglew-dev
+echo "Done!"
+fi
+
+confirm "Java [Javalang]"
+if [ $? -eq 0 ]
+then
+#Install Java
+echo "Installing Java..."
+sudo apt instal default-jdk
+echo "Done!"
+confirm "Kotlin [Kotlinlang] (Runs In Java)"
+if [ $? -eq 0 ]
+then
+#Install Kotlin
+echo "Installing Kotlin..."
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install kotin
+echo "Done!"
+fi
+fi
+
+confirm "Go [GoLang]"
+if [ $? -eq 0 ]
+then
+#Setup GO
+cd ~ #Main Folder
+echo "Setting Up Go..."
+wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+echo "Done!"
+fi
+
+confirm "Rust [Rustlang]"
+if [ $? -eq 0 ]
+then
+#Setup Rust
+cd ~ #Main Folder
+echo "Setting Up Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "Done!"
+fi
+
+# Show Current Sttroage Capacity
+echo "Remaining Storage:"
+df -h
+
+echo "[Program Ended]"
+exit 0
